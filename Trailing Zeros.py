95fcea4b3d1365644825748539a22651833977a6
madhujammm <155441219+madhujammm@users.noreply.github.com>
2025-02-22 23:34:12 +0530

    Update Trailing Zeros.py

--- Trailing Zeros.py
+++ Trailing Zeros.py
@@ -5,6 +5,6 @@ def trailing_zeros(n):
         count += n
     return count
 '''
-Trailing zeros are determined by the number of times n! is divisible by powers of 5.
-Recursive implementation is unnecessary here because the calculation is iterative.
+Trailing zeros are determined by the number of times n! is divisible by powers of 5
+Recursive implementation is unnecessary here because the calculation is iterative
 '''
